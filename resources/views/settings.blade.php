23a447a25f067fde52ad47d1f52ef82e59bb7f6c
musmula <oliver.dvorski@gmail.com>
2016-11-21 16:16:07 +0100

    Add the danger zone to the settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -5,6 +5,14 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            
+            <div class="alert alert-info alert-dismissible" role="alert">
+                Want to change your profile picture?
+                We pull those from <a href="https://en.gravatar.com/" target="_blank" class="alert-link">gravatar.com</a>
+                (Actually we don't... not yet anyway)
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+
             <div class="panel panel-default">
                 <div class="panel-heading">Your basic account information</div>
 
@@ -70,15 +78,17 @@
                         </div>
 
                     </form>
+                </div>  
+            </div> {{-- Panel end --}}
+            
+            <div class="panel panel-danger">
+                <div class="panel-heading">Danger zone</div>
+                <div class="panel-body">
+                    <p>Once you delete your account, there is no going back. Please be certain.</p>
+                    <button class="btn btn-danger">Delete my account</button>
                 </div>
             </div>
 
-            <div class="alert alert-info alert-dismissible" role="alert">
-                Want to change your profile picture?
-                We pull those from <a href="http://gravatar.com" target="_blank" class="alert-link">gravatar.com</a>
-                (Actually we don't... not yet anyway)
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            </div>
         </div>
     </div>
 </div>

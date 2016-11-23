46480b4b117171c34c39bc7336e78cc1bfac87b7
musmula <oliver.dvorski@gmail.com>
2016-11-24 00:55:25 +0100

    Add empty queue message when accessing the petQueue.index view

--- resources/views/petQueue/index.blade.php
+++ resources/views/petQueue/index.blade.php
@@ -5,18 +5,26 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Registered pets:</div>
-                    <ul class="list-group">
-                        @foreach($queue as $item)
-                            <li class="list-group-item">
-                                <a href="{{ url('/pets/queue/' . $item->id) }}">
-                                    {{ $item->name }}
-                                </a>
-                                <span class="pull-right"> {{ $item->type }} </span>
-                                <div class="clearfix"></div>
-                            </li>
-                        @endforeach()
-                    </ul>
+                <div class="panel-heading">Queue item:</div>
+                    @if(count($queue) > 0)
+                        <ul class="list-group">
+                            @foreach($queue as $item)
+                                <li class="list-group-item">
+                                    <a href="{{ url('/pets/queue/' . $item->id) }}">
+                                        {{ $item->name }}
+                                    </a>
+                                    <span class="pull-right"> {{ $item->type }} </span>
+                                    <div class="clearfix"></div>
+                                </li>
+                            @endforeach()
+                        </ul>
+
+                    @else
+                        <div class="panel-body">
+                            The queue is empty
+                        </div>
+
+                    @endif
             </div> {{-- Panel end --}}
 
         </div>

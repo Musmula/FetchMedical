d0383b4708dd9d61075c169f92d8c458a73099f3
musmula <oliver.dvorski@gmail.com>
2016-11-24 00:56:08 +0100

    Typo fix

--- resources/views/petQueue/index.blade.php
+++ resources/views/petQueue/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Queue item:</div>
+                <div class="panel-heading">Queue items:</div>
                     @if(count($queue) > 0)
                         <ul class="list-group">
                             @foreach($queue as $item)

6dbc0d88900d08d8185739d8c2c4fa666540ce43
musmula <oliver.dvorski@gmail.com>
2016-12-12 14:45:45 +0100

    Make the download button on the pet.index pet components conditional

--- resources/assets/js/components/Pet.vue
+++ resources/assets/js/components/Pet.vue
@@ -8,13 +8,16 @@
                 <i class="fa fa-eye"></i>
             </a>
 
-            <a :href="url + '/edit'" class="btn btn-default">
+            <a :href="url + '/edit'" class="btn btn-default" :style="rightBorder">
                 <i class="fa fa-edit"></i>
             </a>
 
-            <a :href="url + '/pdf/download'" class="btn btn-default">
-                <i class="fa fa-download"></i>
-            </a>
+            <div v-if="status == 'Up to date'" style="display: inline">
+                <a :href="url + '/pdf/download'" class="btn btn-default">
+                    <i class="fa fa-download"></i>
+                </a>
+            </div>
+
         </div>
 
         <i class="fa fa-refresh" title="Queued for processing" v-show="displayStatus"></i>
@@ -29,6 +32,10 @@
         computed: {
             displayStatus() {
                 return this.status == 'Queued for processing' ? true : false;
+            },
+
+            rightBorder() {
+                return this.status == 'Up to date' ? 'border-right: 0' : null;
             }
         }
     }

fc35903e4c725130a158cdd937ecb5c6a2ff28a9
musmula <oliver.dvorski@gmail.com>
2017-03-31 21:06:45 +0200

    Remove more of the homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,41 +10,5 @@
         <div class="hero">
             <h2>Online Medical Records for Your Pet</h2>
         </div>
-
-        <div class="process">
-            <h2>Pet Medical records - Simplified</h2>
-
-            <div class="item">
-                <img src="{{ url('/img/process/1.jpg') }}">
-                <div class="clearfix"></div>
-                <span class="number">1</span>
-                <p class="text">
-                    Input your pet's information, including name of vet
-                </p>
-            </div>
-
-            <div class="item">
-                <img src="{{ url('/img/process/2.jpg') }}">
-                <div class="clearfix"></div>
-                <span class="number">2</span>
-                <p class="text">
-                    Our team will contact your vet and compile your pet's medical history
-                </p>
-            </div>
-
-            <div class="item">
-                <img src="{{ url('/img/process/3.jpg') }}">
-                <div class="clearfix"></div>
-                <span class="number">3</span>
-                <p class="text">
-                    Keep and share your pet's information
-                </p>
-            </div>
-        </div>
-
-        <div class="contact">
-            <a href="mailto:hello@fetchmedical.com">hello@fetchmedical.com</a>
-        </div>
-
     </div>
 @stop
